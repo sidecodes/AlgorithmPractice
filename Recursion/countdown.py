cc3563e78c9fb383acc9e8676d8ba6f73d2e28b3
sidecodes <sidekicktal@outlook.com>
2020-10-14 01:01:14 +0530

    Power

---
+++ Recursion/countdown.py
@@ -0,0 +1,12 @@
+# recursion to implement a countdown counter
+
+def countdown(x):
+    if x == 0:
+        print("Done!")
+        return
+    else:
+        print(x, "...")
+        countdown(x-1)
+
+
+countdown(5)
