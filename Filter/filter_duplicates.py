5e03d6c19e77738c5b4912bacfa70667322cdff3
sidecodes <sidekicktal@outlook.com>
2020-10-14 01:05:37 +0530

    Filter Duplicate

---
+++ Filter/filter_duplicates.py
@@ -0,0 +1,17 @@
+# use a hashtable to filter out duplicate items
+
+# define a set of items with duplicates
+items = ["apple", "pear", "orange", "banana", "apple",
+         "orange", "apple", "pear", "banana", "orange",
+         "apple", "kiwi", "pear", "apple", "orange"]
+
+# create a hashtable to perform a filter
+filter = dict()
+
+# loop over each item and add to the hashtable
+for item in items:
+    filter[item] = 0
+
+# create a set from the resulting keys in the hashtable
+result = set(filter.keys())
+print(result)
